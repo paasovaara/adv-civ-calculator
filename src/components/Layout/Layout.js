cd1cb1f5c67ede0da5e1d2605c59b65c78559bc0
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-01-02 21:12:27 +0200

    Cleanup and basic boilerplate

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+
+import classes from './Layout.css';
+
+const layout = (props) => (
+  <Fragment>
+    <h1>Advanced Civ Calculator</h1>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
+      {props.children}
+    </main>
+  </Fragment>
+);
+
+export default layout;
