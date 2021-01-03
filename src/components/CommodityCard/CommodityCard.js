6b921e3ec8ca9a98e4a7deed8de4de4386b83331
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-01-03 11:15:29 +0200

    Add and dec buttons

--- src/components/CommodityCard/CommodityCard.js
+++ src/components/CommodityCard/CommodityCard.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import classes from "./CommodityCard.module.css";
 
-const commodityCard = (props) => {
+
+const CommodityCard = (props) => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className={classes.CommodityCard}>
-      <p>{props.type}</p>
+      <h2>{props.type}</h2>
+      <button onClick={() => count > props.max ? setCount(props.max): setCount(count + 1)} >
+        +
+      </button>
+      <p>{count}</p>
+      <button onClick={() => count > 0 ? setCount(count - 1): setCount(0)}> 
+        -
+      </button>
     </div>
   );
 }
 
-export default commodityCard;
+export default CommodityCard;
