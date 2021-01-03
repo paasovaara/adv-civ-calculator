a52f293509196305425868b789cd63f979b5f302
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-01-03 09:47:59 +0200

    Wallet Layout to responsive two cols

--- src/components/WalletLayout/WalletLayout.js
+++ src/components/WalletLayout/WalletLayout.js
@@ -7,15 +7,21 @@ import classes from "./WalletLayout.module.css";
 const WalletLayout = () => {
   //TODO remove extra div for columns, just add the class to CommodityCard?
   return (
-    <div className={classes.row}>
-      <div className={classes.column}>
+    <div className={classes.WalletLayout}>
+      <div className={classes.left}>
+        <CommodityCard type={CommodityTypes.Ochre} />       
+      </div>
+      <div className={classes.right}>
+        <CommodityCard type={CommodityTypes.Hides} />       
+      </div>
+      <div className={classes.left}>
         <CommodityCard type={CommodityTypes.Bronze} />       
       </div>
-      <div className={classes.column}>
+      <div className={classes.right}>
         <CommodityCard type={CommodityTypes.Dye} />       
       </div>
-      <div className={classes.column}>
-        <CommodityCard type={CommodityTypes.Ochre} />       
+      <div className={classes.left}>
+        <CommodityCard type={CommodityTypes.Gold} />       
       </div>
     </div>
   );
