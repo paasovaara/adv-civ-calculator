1b184e1443b4ec5ff99d89ab6a8a55f8e6e2a77f
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-01-09 14:41:53 +0200

    Clean walletLAyout code and add unique keys to list

--- src/components/WalletLayout/WalletLayout.js
+++ src/components/WalletLayout/WalletLayout.js
@@ -5,64 +5,25 @@ import CommodityCard from "../CommodityCard/CommodityCard";
 import classes from "./WalletLayout.module.css";
 
 const WalletLayout = () => {
+  const createCommodityCard = (commodityType, className) => {
+    return (<div className={className} key={commodityType}>
+      <CommodityCard commodity={getCommodity(commodityType)} />       
+    </div>);
+  };
+
+  let odd = true;
+  const commodityCards = [];
+  for (const type in CommodityTypes) {
+    const className = odd ? classes.left : classes.right;
+    const card = createCommodityCard(CommodityTypes[type], className);
+    commodityCards.push(card);
+    odd = !odd;
+  }
+
   return (
     <div className={classes.WalletLayout}>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Ochre)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Hides)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Iron)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Papyrus)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Salt)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Timber)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Grain)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Oil)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Cloth)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Wine)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Bronze)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Silver)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Spices)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Resin)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Gems)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Dye)} />       
-      </div>
-      <div className={classes.left}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Gold)} />       
-      </div>
-      <div className={classes.right}>
-        <CommodityCard commodity={getCommodity(CommodityTypes.Ivory)} />       
-      </div>
-    </div>
-  );
+      { commodityCards }
+    </div>);
 }
 
 export default WalletLayout;
