ec9670fc827107bea8078c4d516057fbb6fdc42b
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-01-17 21:56:50 +0200

    Bugfix: calc correctly

--- src/components/WalletLayout/WalletLayout.js
+++ src/components/WalletLayout/WalletLayout.js
@@ -5,18 +5,12 @@ import CommodityCard from "../CommodityCard/CommodityCard";
 import classes from "./WalletLayout.module.css";
 
 const WalletLayout = () => {
-  /*
-  const cardCounts = [];
-  for (const [key, value] of Object.entries(Commodities)) {
-    console.log(`${key}: ${value}`);
-    cardCounts.push({ [key]: 0});
-  }*/
+  
   const cardCounts = {};
   for (const [key, value] of Object.entries(Commodities)) {
     cardCounts[key] = 0;
   }
 
-  //const cardCounts = Object.entries(Commodities).map((type, commodity) => (type, 0));
   console.log(cardCounts);
   const walletState = {
     cardCounts: cardCounts,
@@ -26,15 +20,13 @@ const WalletLayout = () => {
   const [wallet, setWallet] = useState(walletState);
 
   const incHandler = (commodity) => {
-    console.log("inchandler called");
-    
     const counts = {...wallet.cardCounts};
     console.log(counts);
-    const oldCount = counts[commodity.type].count;
+    const oldCount = counts[commodity.type];
     console.log(commodity.type, oldCount);
     
     if (oldCount >= commodity.count) {
-      console.log(`Error, incHandler called for commodity ${commodity} with too large value ${oldCount}`);
+      console.error(`WalletLayout:incHandler called for commodity ${commodity} with too large value ${oldCount}`);
       return;
     }
 
@@ -42,15 +34,15 @@ const WalletLayout = () => {
     counts[commodity.type] = newCount;
 
     const oldTotalMoney = wallet.money;
-    const oldValue = commodity.values[oldCount];
-    const newValue = commodity.values[newCount];
+    const oldValue = (oldCount > 0) ? commodity.values[oldCount - 1] : 0;//Ugly, TODO fix
+    const newValue = commodity.values[newCount - 1]; //Ugly, TODO fix
     const newTotalMoney = oldTotalMoney - oldValue + newValue;
 
     const newWallet = { money: newTotalMoney, cardCounts: counts};
     setWallet(newWallet);
   }
 
-  //TODO give countSate and the setter to each CommodityCard and replace the state over there..
+  //TODO add also reduce handler and remove the state from CommodityCard
   const createCommodityCard = (commodityType, className) => {
     return (<div className={className} key={commodityType}>
       <CommodityCard 
